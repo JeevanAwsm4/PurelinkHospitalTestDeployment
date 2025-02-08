b1931c0697b10d7ca804c33a3566eb9057afe19e
irshadirshu0722 <muhammedirshadismail0722@gmail.com>
2025-02-08 14:52:48 +0530

    home page designed

---
+++ src/@db/home.ts
@@ -0,0 +1,85 @@
+import CardIcon1 from "@/assets/svg/statistic_card1.svg";
+import CardIcon2 from "@/assets/svg/statistic_card2.svg";
+import CardIcon3 from "@/assets/svg/statistic_card3.svg";
+import CardBg1 from "@/assets/images/statistic-card-bg1.png";
+import CardBg2 from "@/assets/images/statistic-card-bg2.png";
+
+export const homeCards = [
+  {
+    icon: CardIcon1,
+    heading: "Request This Month",
+    arrowDirection: "top",
+    theme: "primary",
+    count: 179,
+    highlight: "12 requests",
+    description: "More than last month",
+    highlight_type: "positive",
+    bg: CardBg2,
+  },
+  {
+    icon: CardIcon2,
+    heading: "Donors in city",
+    arrowDirection: "bottom",
+    theme: "primary",
+    count: 179,
+    highlight: "12 requests",
+    description: "More than last month",
+    highlight_type: "negative",
+    bg: CardBg1,
+  },
+  {
+    icon: CardIcon3,
+    heading: "Total request in city",
+    arrowDirection: "bottom",
+    theme: "primary",
+    count: 179,
+    highlight: "12 requests",
+    description: "More than last month",
+    highlight_type: "positive",
+    bg:CardBg2
+  },
+];
+
+
+export const requestData = [
+  {
+    request_id: "#D92D20",
+    blood_type: "RBC",
+    donor_count: 1,
+    status: "pending",
+    slot_duration: "3PM - 7 PM",
+    date: "15/02/2024",
+  },
+  {
+    request_id: "#D92D20",
+    blood_type: "RBC",
+    donor_count: 1,
+    status: "pending",
+    slot_duration: "3PM - 7 PM",
+    date: "15/02/2024",
+  },
+  {
+    request_id: "#D92D20",
+    blood_type: "RBC",
+    donor_count: 1,
+    status: "pending",
+    slot_duration: "3PM - 7 PM",
+    date: "15/02/2024",
+  },
+  {
+    request_id: "#D92D20",
+    blood_type: "RBC",
+    donor_count: 1,
+    status: "pending",
+    slot_duration: "3PM - 7 PM",
+    date: "15/02/2024",
+  },
+  {
+    request_id: "#D92D20",
+    blood_type: "RBC",
+    donor_count: 1,
+    status: "pending",
+    slot_duration: "3PM - 7 PM",
+    date: "15/02/2024",
+  },
+];
--- src/components/atomic/button/Button.tsx
+++ src/components/atomic/button/Button.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-interface Props {
+interface Props  {
   onClick : () => void;
   disabled : boolean;
   children : React.ReactNode;
+  className :string
 }
-const Button = ({children,disabled,onClick}: Props) => {
+const Button = ({ children, disabled, onClick, className }: Props) => {
   const bg = !disabled ? "bg-[#7464F0]" : "bg-gray-300";
   return (
     <button
       onClick={onClick}
-      className={`${bg} w-full rounded-lg p-3 text-[1rem] font-semibold leading-[1.5rem] text-center text-white`}
+      className={`${bg} w-full rounded-lg p-3 text-[1rem] font-semibold leading-[1.5rem] text-center text-white ${className}`}
     >
       {children}
     </button>
